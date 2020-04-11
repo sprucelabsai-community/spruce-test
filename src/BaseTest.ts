72e25f5ea254a5694f92a8dbdf6d5d970a3d0f9f
liquidg3 <me@taylorrome.ro>
2020-04-11 16:56:54 -0600

    feat: base tests have resolvepath helper

--- src/BaseTest.ts
+++ src/BaseTest.ts
@@ -1,6 +1,31 @@
+import path from 'path'
+
 /** Base test class chalk full of helpers to make testing more 🔥🔥🔥 */
 export default class BaseTest {
-	protected static async wait(ms: number) {
+	/** Resolve a local file relative to the test being run */
+	public static resolvePath(...filePath: string[]) {
+		// Paths should be resolved relative to the test file vs the root of the project
+		const cache = require('module')._cache
+		const matchedModule = Object.keys(cache)
+			.map(key => cache[key])
+			.find(item => item?.exports === this || item?.exports?.default === this)
+
+		const base = path.dirname(matchedModule.filename)
+		let builtPath = path.join(...filePath)
+
+		if (builtPath[0] !== '/') {
+			// Relative to the cwd
+			if (builtPath.substr(0, 2) === './') {
+				builtPath = builtPath.substr(1)
+			}
+
+			builtPath = path.join(base, builtPath)
+		}
+
+		return builtPath
+	}
+	/** Hold for a sec  */
+	protected static async wait(ms = 1000) {
 		return new Promise(resolve => {
 			setTimeout(() => resolve(), ms)
 		})
