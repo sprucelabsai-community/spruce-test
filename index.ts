9331f3ffca73afee8e5a27ec101e980ec2b6d419
liquidg3 <me@taylorrome.ro>
2020-07-06 19:43:28 -0600

    patch: build only src files

--- index.ts
+++
@@ -1,5 +0,0 @@
-import AbstractSpruceTest from './src/AbstractSpruceTest'
-export { default as assert } from './src/assert'
-export * from './src/assert'
-export default AbstractSpruceTest
-export { default as test } from './src/decorators'
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import AbstractSpruceTest from './AbstractSpruceTest'
+export { default as assert } from './assert'
+export * from './assert'
+export default AbstractSpruceTest
+export { default as test } from './decorators'
