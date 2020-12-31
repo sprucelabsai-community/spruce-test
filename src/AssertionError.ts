c13baa88b6f2ca4b27d44e4bb6ce29625a7929fa
Taylor Romero <me@taylorrome.ro>
2020-12-31 16:57:04 -0700

    patch: drop the stack

--- src/AssertionError.ts
+++ src/AssertionError.ts
@@ -3,7 +3,7 @@ import StackCleaner from './StackCleaner'
 export default class AssertionError extends Error {
 	public constructor(message: string, stack?: string) {
 		super(message)
-		this.message = StackCleaner.clean(message ?? this.message ?? '')
+		this.message = StackCleaner.clean(message ?? this.message ?? '') + '\n\n\n'
 		this.stack = StackCleaner.clean(stack ?? this.stack ?? '')
 	}
 }
