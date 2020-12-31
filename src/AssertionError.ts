fdfb93097f64a90a2bf4693c38209341840ac850
Taylor Romero <me@taylorrome.ro>
2020-12-31 16:22:05 -0700

    patch: back to original stack cleaning

--- src/AssertionError.ts
+++ src/AssertionError.ts
@@ -3,7 +3,6 @@ import StackCleaner from './StackCleaner'
 export default class AssertionError extends Error {
 	public constructor(message: string, stack?: string) {
 		super(message)
-		this.message = StackCleaner.clean(stack ?? this.stack ?? message)
-		this.stack = '\n'
+		this.stack = StackCleaner.clean(stack ?? this.stack ?? message)
 	}
 }
