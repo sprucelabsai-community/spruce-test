fbab6f894d40608ae0680b1d2eee05126cc3f862
Taylor Romero <me@taylorrome.ro>
2021-01-09 08:15:38 -0700

    patch: node_module filtering on stack cleaner

--- src/AssertionError.ts
+++ src/AssertionError.ts
@@ -3,7 +3,7 @@ import StackCleaner from './StackCleaner'
 export default class AssertionError extends Error {
 	public constructor(message: string, stack?: string) {
 		super(message)
-		this.message = StackCleaner.clean(message ?? this.message ?? '') + '\n\n\n'
+		this.message = StackCleaner.clean(message ?? this.message ?? '') + '\n'
 		this.stack = StackCleaner.clean(stack ?? this.stack ?? '')
 	}
 }
