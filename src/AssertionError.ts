e084a427c5c7dbe60ac91fdf36ff3ecaa1eccc2a
Taylor Romero <me@taylorrome.ro>
2021-01-20 09:34:26 -0700

    patch: message in stack until jest updates

--- src/AssertionError.ts
+++ src/AssertionError.ts
@@ -3,7 +3,9 @@ import StackCleaner from './StackCleaner'
 export default class AssertionError extends Error {
 	public constructor(message: string, stack?: string) {
 		super(message)
-		this.message = StackCleaner.clean(message ?? this.message ?? '') + '\n'
-		this.stack = StackCleaner.clean(stack ?? this.stack ?? '')
+		this.message = StackCleaner.clean(message ? `${message}\n` : '')
+		this.stack = StackCleaner.clean(
+			`${this.message}${stack ?? this.stack ?? ''}`
+		)
 	}
 }
