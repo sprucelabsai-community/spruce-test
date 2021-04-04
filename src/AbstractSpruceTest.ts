da842a839db47f2ff2d8c071426ea2ee51e9c695
Taylor Romero <me@taylorrome.ro>
2021-04-03 19:17:54 -0600

    minor: log available on abstract test to write to stderr

--- src/AbstractSpruceTest.ts
+++ src/AbstractSpruceTest.ts
@@ -1,33 +1,16 @@
 import path from 'path'
 
-export interface ITestPlugin {
-	afterAll?: () => void
-}
-
-/** Base test class chalk full of helpers to make testing more 🔥🔥🔥 */
 export default class AbstractSpruceTest {
-	/** The current cwd */
 	protected static cwd: string
 
-	private static plugins: ITestPlugin[] = []
-
-	/** Override this method to execute code before all your tests */
 	protected static async beforeAll() {
 		this.cwd = process.cwd()
 	}
 
-	/** Override this method to execute code after all your tests */
-	protected static async afterAll() {
-		this.plugins.forEach((plugin) => plugin.afterAll?.())
-	}
-
-	/** Override this method to execute code before each of your tests run */
+	protected static async afterAll() {}
 	protected static async beforeEach() {}
-
-	/** Override this method to execute code after each of your tests run */
 	protected static async afterEach() {}
 
-	/** Resolve a local file relative to the test being run */
 	protected static resolvePath(...filePath: string[]) {
 		const cwd = this.cwd
 		let builtPath = path.join(...filePath)
@@ -37,7 +20,6 @@ export default class AbstractSpruceTest {
 		}
 
 		if (builtPath[0] !== '/') {
-			// Relative to the cwd
 			if (builtPath.substr(0, 2) === './') {
 				builtPath = builtPath.substr(1)
 			}
@@ -48,19 +30,14 @@ export default class AbstractSpruceTest {
 		return builtPath
 	}
 
-	/** Hold for a sec  */
 	protected static async wait(ms = 1000) {
 		return new Promise((resolve) => {
 			setTimeout(() => resolve(true), ms)
 		})
 	}
 
-	/** Plugins can listen in to all hooks and tests and do whatever else they need */
-	protected static addPlugin(plugin: ITestPlugin) {
-		this.plugins.push(plugin)
-	}
-
-	protected static async clearPlugins() {
-		this.plugins = []
+	protected static log(...args: any[]) {
+		const str = args.map((a) => `${a}`).join(' ')
+		process.stderr.write(str)
 	}
 }
